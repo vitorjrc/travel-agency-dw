4ebea54f5ef78115928c3f167438b1bd87b07021
Vitor Castro <v.j.r.c1997@hotmail.com>
2019-01-20 17:45:35 +0000

    PENTAHO AR Quarantine

--- area de retenção/arConsulting.sql
+++ area de retenção/arConsulting.sql
@@ -19,6 +19,9 @@ limit 0, 5000;
 select * from ardw.ar_cleanup_cliente_s
 limit 0, 5000;
 
+-- teste null
+insert into ardw.ar_cleanup_cliente_s values (26, 999999999, "Picheleiro", "Vitoriaaaaa", null);
+
 select * from ardw.ar_cleanup_vendas_s
 limit 0, 5000;
 
@@ -41,3 +44,10 @@ limit 0, 5000;
 
 select * from ardw.ar_cleanup_viagens_e_previous
 limit 0, 5000;
+
+
+-- quarentena
+select * from ardw.ar_dim_calendario;
+select * from ardw.ar_dim_cliente;
+select * from ardw.ar_dim_vendas;
+select * from ardw.ar_dim_viagens;
