20809b9d88141d775438374096a3e993228804b8
Vitor Castro <v.j.r.c1997@hotmail.com>
2019-01-20 16:34:02 +0000

    PENTAHO DW Fixing refreshment for fact table

--- area de retenção/arConsulting.sql
+++ area de retenção/arConsulting.sql
@@ -11,6 +11,9 @@ limit 0, 5000;
 select * from ardw.ar_cleanup_viagens_m
 limit 0, 5000;
 
+select * from ardw.ar_cleanup_viagens_m_previous
+limit 0, 5000;
+
 
 -- mysql
 select * from ardw.ar_cleanup_cliente_s
@@ -22,6 +25,9 @@ limit 0, 5000;
 select * from ardw.ar_cleanup_viagens_s
 limit 0, 5000;
 
+select * from ardw.ar_cleanup_viagens_s_previous
+limit 0, 5000;
+
 
 -- excel
 select * from ardw.ar_cleanup_cliente_e
@@ -31,4 +37,7 @@ select * from ardw.ar_cleanup_vendas_e
 limit 0, 5000;
 
 select * from ardw.ar_cleanup_viagens_e
+limit 0, 5000;
+
+select * from ardw.ar_cleanup_viagens_e_previous
 limit 0, 5000;
